406eb8a24edddde86aa3b85aac829dfe93f4383a
Anil Rai <anilrai4001@gmail.com>
2024-03-15 19:00:43 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log('connected to mongodb!')
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
+
+
 const app = express()
 
 
-app.listen(5000, ()=>{
-    console.log(`server started on port: 5000!`)
+app.listen(5000, () => {
+    console.log(`server started on port: 5000`)
 })
